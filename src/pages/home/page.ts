3552dd1f2ac83de9c37f0a04949fa7657220b4e3
순민 <tkksm2@gmail.com>
2024-02-14 16:57:41 +0900

    style: add Header component

--- src/pages/home/page.ts
+++ src/pages/home/page.ts
@@ -1,21 +1,16 @@
 import Component from "@/lib/dom";
-import Router from "@/lib/router";
+import { BaseLayout } from "../layouts";
 
 class Home extends Component {
   protected render(): string {
     return `
-    <h1>Home Page</h1>
-    <ul>
-      <li><a href="/about/10" data-link>go About</a></li>
-      <li><a href="/login" data-link>go Login</a></li>
-    </ul>
-    <button>go About!!</button>
+      <div data-component="BaseLayout"></div>
     `;
   }
-  protected setEvent(): void {
-    this.attachEvent("button", "click", () => {
-      // Router.replace(`/about/${Math.ceil(Math.random() * 10)}`);
-      Router.pop();
+  protected mounted(): void {
+    const $el = document.querySelector("[data-component=BaseLayout]")!;
+    new BaseLayout($el, {
+      children: `<div>contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent</div>`,
     });
   }
 }
---
+++ src/pages/layouts/BaseLayout/BaseLayout.ts
@@ -0,0 +1,32 @@
+import Component from "@/lib/dom";
+import Router from "@/lib/router";
+import AppHeader from "./components/AppHeader";
+import AppFooter from "./components/AppFooter";
+
+interface BaseLayoutProps {
+  children: string;
+}
+
+class BaseLayout extends Component<BaseLayoutProps> {
+  protected render(): string {
+    return `  
+      <div data-component="AppHeader"></div>
+      <main>${this.props.children}</main>
+      <div data-component="AppFooter"></div>
+    `;
+  }
+  protected mounted(): void {
+    const $AppHeader = document.querySelector("[data-component=AppHeader]")!;
+    const $AppFooter = document.querySelector("[data-component=AppFooter]")!;
+    new AppHeader($AppHeader);
+    new AppFooter($AppFooter);
+  }
+  protected setEvent(): void {
+    this.attachEvent("button", "click", () => {
+      // Router.replace(`/about/${Math.ceil(Math.random() * 10)}`);
+      Router.pop();
+    });
+  }
+}
+
+export default BaseLayout;
