d32f711a725d5054ff6210966ee92aedd4de657b
SoonMin <89559826+Ssoon-m@users.noreply.github.com>
2024-02-21 14:05:26 +0900

    Revert "[style] 기본 Layout 잡기"

--- src/pages/home/page.ts
+++ src/pages/home/page.ts
@@ -1,16 +1,21 @@
 import Component from "@/lib/dom";
-import { BaseLayout } from "../layouts";
+import Router from "@/lib/router";
 
 class Home extends Component {
   protected render(): string {
     return `
-      <div data-component="BaseLayout"></div>
+    <h1>Home Page</h1>
+    <ul>
+      <li><a href="/about/10" data-link>go About</a></li>
+      <li><a href="/login" data-link>go Login</a></li>
+    </ul>
+    <button>go About!!</button>
     `;
   }
-  protected mounted(): void {
-    const $el = document.querySelector("[data-component=BaseLayout]")!;
-    new BaseLayout($el, {
-      children: `<div>content??</div>`,
+  protected setEvent(): void {
+    this.attachEvent("button", "click", () => {
+      // Router.replace(`/about/${Math.ceil(Math.random() * 10)}`);
+      Router.pop();
     });
   }
 }
